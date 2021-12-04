47725e22e8935f1a249d7d8322d872c3cf60c0a6
Maurits <mauritsseelen@hotmail.com>
2021-12-04 22:31:12 +0100

    .net 6

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,27 +1,17 @@
-﻿using System.IO;
-using System.Linq;
+﻿using AdventOfCode;
 
-namespace AdventOfCode
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var day1 = new Day1();
-            day1.Solution1();
-            day1.Solution2();
+var day1 = new Day1();
+day1.Solution1();
+day1.Solution2();
 
-            var day2 = new Day2();
-            day2.Solution1();
-            day2.Solution2();
+var day2 = new Day2();
+day2.Solution1();
+day2.Solution2();
 
-            var day3 = new Day3();
-            day3.Solution1();
-            day3.Solution2();
+var day3 = new Day3();
+day3.Solution1();
+day3.Solution2();
 
-            var day4 = new Day4();
-            day4.Solution1();
-            day4.Solution2();
-        }
-    }
-}
+var day4 = new Day4();
+day4.Solution1();
+day4.Solution2();
