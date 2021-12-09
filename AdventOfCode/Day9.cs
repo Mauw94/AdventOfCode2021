c43242f2a67e34090e5e750a8ec7ebcf1a86770f
Maurits Seelen <mauritsseelen@gmail.com>
2021-12-09 10:36:44 +0100

    not checking down or up.. have to continue real work now rip

---
+++ AdventOfCode/Day9.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public class Day9 : Base
+    {
+        private List<int> _locationPoints;
+
+        public Day9()
+        {
+            _locationPoints = new();
+            string[] input = File.ReadAllLines("C:\\Projects\\AdventOfCode2021\\input\\day9.txt");
+            foreach (var line in input)
+            {
+                foreach (var point in line)
+                {
+                    _locationPoints.Add(int.Parse(point.ToString()));
+                }
+            }
+        }
+
+        public override void Solution1()
+        {
+            var lowPoints = 0;
+            for (int i = 0; i < _locationPoints.Count; i++)
+            {
+                if (i == 0 || i == _locationPoints.Count) continue;
+
+                if (_locationPoints[i] < _locationPoints[i - 1] && _locationPoints[i] < _locationPoints[i + 1])
+                    lowPoints++;
+            }
+        }
+
+        public override void Solution2()
+        {
+
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -30,6 +30,9 @@
 //day7.Solution2();
 //day7.EasierSolution2();
 
-var day8 = new Day8();
-day8.Solution1();
-day8.Solution2();
+//var day8 = new Day8();
+//day8.Solution1();
+//day8.Solution2();
+
+var day9 = new Day9();
+day9.Solution1();
