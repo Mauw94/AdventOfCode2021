0edfbff28d18142dd2820d171bcaff825a549724
Maurits Seelen <mauritsseelen@gmail.com>
2021-12-16 17:00:15 +0100

    welp..

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -50,6 +50,9 @@
 //day12.Solution1();
 //day12.Solution2();
 
-var day13 = new Day13();
-day13.Solution1();
-day13.Solution2();
+//var day13 = new Day13();
+//day13.Solution1();
+//day13.Solution2();
+
+var day14 = new Day14();
+day14.Solution1();
