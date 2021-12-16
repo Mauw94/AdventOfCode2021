310a8469b5e4ef71b0f6ef4b90915941a9f8a498
Maurits Seelen <mauritsseelen@gmail.com>
2021-12-16 14:51:12 +0100

    work in progress

--- AdventOfCode/AoC/Day13.cs
+++ AdventOfCode/AoC/Day13.cs
@@ -10,9 +10,24 @@ namespace AdventOfCode2021.AoC
     {
         private readonly List<Coord> _coordinates;
         private readonly List<FoldAt> _folds;
+        private Paper[,] _paper;
 
-        record Coord(int Row, int Col);
+        record Coord(int X, int Y);
         record FoldAt(char Axis, int Position);
+
+        class Paper
+        {
+            public Coord Coord { get; set; }
+            public bool IsFolded { get; set; }
+            public bool Remove { get; set; }
+
+            public Paper(Coord coord, bool isFolded)
+            {
+                Coord = coord;
+                IsFolded = isFolded;
+                Remove = false;
+            }
+        }
 
         public Day13()
         {
@@ -22,8 +37,123 @@ namespace AdventOfCode2021.AoC
             List<string> input = Common.GetInput(13);
 
             ParseAllCoordinates(input);
-
             ParseAllFolds(input);
+            CreatePaper();
+            Fold();
+        }
+
+        void Fold()
+        {
+            foreach (var fold in _folds)
+            {
+                if (fold.Axis == 'y')
+                    FoldUp(fold.Position);
+                else
+                    FoldLeft(fold.Position);
+            }
+        }
+
+        void FoldUp(int position)
+        {
+            var foldCount = 0;
+            for (int i = 0; i < _paper.GetLength(0); i++)
+            {
+                if (i > position) foldCount += 2;
+                for (int j = 0; j < _paper.GetLength(1); j++)
+                {
+                    if (i > position)
+                    {
+                        if (!(_paper[i - foldCount, j].IsFolded) && _paper[i, j].IsFolded)
+                            _paper[i - foldCount, j].IsFolded = true;
+
+                        _paper[i, j].Remove = true;
+                    }
+                }
+            }
+
+            // Add 1 cos we also want to remove at the current position.
+            CleanUpPaper(position, true);
+            PrintFolderPaper();
+
+        }
+
+        void FoldLeft(int position)
+        {
+            var foldCount = 0;
+            for (int i = 0; i < _paper.GetLength(0); i++)
+            {
+                foldCount = 0;
+                for (int j = 0; j < _paper.GetLength(1); j++)
+                {
+                    foldCount += 2;
+                    if (j > position)
+                    {
+                        if (!(_paper[i, j - foldCount].IsFolded) && _paper[i, j].IsFolded)
+                            _paper[i, j - foldCount].IsFolded = true;
+
+                        _paper[i, j].Remove = true;
+                    }
+                }
+            }
+
+            // Add 1 cos we also want to remove at the current position.
+            CleanUpPaper(position + 1, false);
+            PrintFolderPaper();
+
+        }
+
+        void CleanUpPaper(int position, bool foldUp)
+        {
+            Paper[,] newPaper;
+
+            if (foldUp)
+                newPaper = new Paper[_paper.GetLength(0) - position, _paper.GetLength(1)];
+            else
+                newPaper = new Paper[_paper.GetLength(0), _paper.GetLength(1) - position];
+
+            for (int i = 0; i < newPaper.GetLength(0); i++)
+            {
+                for (int j = 0; j < newPaper.GetLength(1); j++)
+                {
+                    newPaper[i, j] = _paper[i, j];
+                }
+            }
+
+            _paper = newPaper;
+        }
+
+        void PrintFolderPaper()
+        {
+            for (int i = 0; i < _paper.GetLength(0); i++)
+            {
+                for (int j = 0; j < _paper.GetLength(1); j++)
+                {
+                    if (_paper[i, j].IsFolded)
+                        Console.Write(string.Format("{0} ", "#"));
+                    else
+                        Console.Write(string.Format("{0} ", "."));
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+        void CreatePaper()
+        {
+            int rowCount = _coordinates.Max(x => x.Y);
+            int colCount = _coordinates.Max(x => x.X);
+            _paper = new Paper[rowCount, colCount];
+
+            for (int i = 0; i < rowCount; i++)
+                for (int j = 0; j < colCount; j++)
+                {
+                    Coord newPaperCoord = new(j, i);
+                    bool isFolded = false;
+
+                    if (_coordinates.Contains(newPaperCoord))
+                        isFolded = true;
+
+                    _paper[i, j] = new Paper(new Coord(j, i), isFolded);
+                }
         }
 
         void ParseAllCoordinates(List<string> input)
